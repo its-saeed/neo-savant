5c145f9c4032ee158f7fe06fdb2342163121892a
Saeed Dadkhah <saeed@zilliqa.com>
2024-04-24 14:23:16 +0330

    Add open/new scilla file buttons

--- src/components/LeftSidebar/LeftSidebar.vue
+++ src/components/LeftSidebar/LeftSidebar.vue
@@ -2,6 +2,15 @@
   <div class="column full-height">
     <q-bar class="bg-dark text-white shadow-2">
       <q-toolbar-title>Files</q-toolbar-title>
+      <q-space/>
+      <div class="q-gutter-xs">
+        <q-btn class="gt-xs" size="10px" flat dense round icon="add">
+          <q-tooltip>New scilla file</q-tooltip>
+        </q-btn>
+        <q-btn class="gt-xs" size="10px" flat dense round icon="file_download" >
+          <q-tooltip>Open scilla file</q-tooltip>
+        </q-btn>
+      </div>
     </q-bar>
     <q-scroll-area class="col">
       <file-selector/>
