fa6c71a4b17d8834e0829e85bcc4ad3116099277
Saeed Dadkhah <saeed@zilliqa.com>
2024-04-25 12:22:27 +0330

    Add a tiny alarm component for user/net not selected.

---
+++ src/components/UserNetworkNotSelectedAlarm.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-badge
+    v-if="networksStore.selected === null || accountsStore.selected === null"
+    color="red"
+    floating
+  >
+    !
+    <q-tooltip>Network and/or Account not selected!</q-tooltip>
+  </q-badge>
+</template>
+
+<script setup>
+import { useNetworksStore } from 'stores/networks';
+import { useAccountsStore } from 'stores/accounts';
+
+const networksStore = useNetworksStore();
+const accountsStore = useAccountsStore();
+</script>
